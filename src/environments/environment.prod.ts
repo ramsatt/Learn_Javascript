0d234b5ad3c6c7b00ab0156bd25b7f4621108a1d
Sathish Kumar R <codingpullingo@gmail.com>
2026-01-30 16:01:20 +0530

    build issues fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCc8okJCp92LIIfQ-igfTAazC-uqi4KVOs",
+    authDomain: "coding-tamilan.firebaseapp.com",
+    projectId: "coding-tamilan",
+    storageBucket: "coding-tamilan.firebasestorage.app",
+    messagingSenderId: "803889368095",
+    appId: "1:803889368095:web:ccbf155a859762db85992a",
+    measurementId: "G-ZXKQN6BX9V"
+  }
 };
