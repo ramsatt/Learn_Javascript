b04c5f4192c67b54b13e9946b95ef8ad1741e4b9
Sathish Kumar R <codingpullingo@gmail.com>
2026-02-03 09:18:44 +0530

    changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
     messagingSenderId: "803889368095",
     appId: "1:803889368095:web:ccbf155a859762db85992a",
     measurementId: "G-ZXKQN6BX9V"
-  }
+  },
+  geminiApiKey: "AIzaSyDmwU5kSbfHX0fGBu3LJE0M9-QOO6j0F4M"
 };
